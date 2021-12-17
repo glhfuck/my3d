fdb8aaa0b9756d12de54f7e71b66d6c2587eac39
glhfuck <danyacyrex@gmail.com>
2021-12-17 16:11:14 +0200

    reorganization: Shape -> Transformable

--- libs/My3dLib/include/Shape.h
+++ libs/My3dLib/include/Shape.h
@@ -1,65 +1,21 @@
-#ifndef MY3D_SRC_SHAPE_H_
-#define MY3D_SRC_SHAPE_H_
+#ifndef MY3D_LIBS_MY3DLIB_SRC_SHAPE_H_
+#define MY3D_LIBS_MY3DLIB_SRC_SHAPE_H_
 
-#include "boost/numeric/ublas/matrix.hpp"
-#include "boost/numeric/ublas/vector.hpp"
-#include "boost/numeric/ublas/assignment.hpp"
-#include "boost/numeric/ublas/io.hpp"
+#include "Transformable.h"
 
-
-class Shape {
-  using matrix = boost::numeric::ublas::matrix<double>;
+class Shape: public Transformable {
   using vector = boost::numeric::ublas::vector<double>;
 
  public:
-  enum Axes {
-    Ox,
-    Oy,
-    Oz
+
+  struct Facet {
+    size_t vertex_idx[3];
+    size_t normal_idx[3];
   };
-
-  enum Coords {
-    Global,
-    Local
-  };
-
-  Shape();
-  explicit Shape(const vector& pos);
-
-  void setPosition(const vector& pos, Coords coord = Coords::Global);
-  void translate(const vector& vec, Coords coord = Coords::Global);
-  void rotate(double angle, Axes axis, Coords coord);
-  void scale(double x, double y, double z, Coords coord = Coords::Local);
 
   std::vector<vector> vertices_;
   std::vector<vector> normals_;
-  matrix M();
- protected:
-  // TODO Maybe add pos?
-
- private:
-  matrix localScale_;
-  matrix localRotate_;
-  matrix localTranslate_;
-  matrix globalScale_;
-  matrix globalRotate_;
-  matrix globalTranslate_;
-
-  void localRotate(double angle, Axes axis);
-  void globalRotate(double angle, Axes axis);
-
-  void localOxRotate(double angle);
-  void localOyRotate(double angle);
-  void localOzRotate(double angle);
-  void globalOxRotate(double angle);
-  void globalOyRotate(double angle);
-  void globalOzRotate(double angle);
-
-  static matrix getTranslationMatrix(const vector& vec);
-  static matrix getScaleMatrix(double x, double y, double z);
-  static matrix getOxRotateMatrix(double angle);
-  static matrix getOyRotateMatrix(double angle);
-  static matrix getOzRotateMatrix(double angle);
+  std::vector<Facet> facets_;
 };
 
-#endif //MY3D_SRC_SHAPE_H_
+#endif //MY3D_LIBS_MY3DLIB_SRC_SHAPE_H_
---
+++ libs/My3dLib/include/Transformable.h
@@ -0,0 +1,61 @@
+#ifndef MY3D_SRC_SHAPE_H_
+#define MY3D_SRC_SHAPE_H_
+
+#include "boost/numeric/ublas/matrix.hpp"
+#include "boost/numeric/ublas/vector.hpp"
+#include "boost/numeric/ublas/assignment.hpp"
+#include "boost/numeric/ublas/io.hpp"
+
+
+class Transformable {
+  using matrix = boost::numeric::ublas::matrix<double>;
+  using vector = boost::numeric::ublas::vector<double>;
+
+ public:
+  enum Axes {
+    Ox,
+    Oy,
+    Oz
+  };
+
+  enum Coords {
+    Global,
+    Local
+  };
+
+  Transformable();
+  explicit Transformable(const vector& pos);
+
+  void setPosition(const vector& pos, Coords coord = Coords::Global);
+  void translate(const vector& vec, Coords coord = Coords::Global);
+  void rotate(double angle, Axes axis, Coords coord);
+  void scale(double x, double y, double z, Coords coord = Coords::Local);
+
+  matrix M();
+
+ private:
+  matrix localScale_;
+  matrix localRotate_;
+  matrix localTranslate_;
+  matrix globalScale_;
+  matrix globalRotate_;
+  matrix globalTranslate_;
+
+  void localRotate(double angle, Axes axis);
+  void globalRotate(double angle, Axes axis);
+
+  void localOxRotate(double angle);
+  void localOyRotate(double angle);
+  void localOzRotate(double angle);
+  void globalOxRotate(double angle);
+  void globalOyRotate(double angle);
+  void globalOzRotate(double angle);
+
+  static matrix getTranslationMatrix(const vector& vec);
+  static matrix getScaleMatrix(double x, double y, double z);
+  static matrix getOxRotateMatrix(double angle);
+  static matrix getOyRotateMatrix(double angle);
+  static matrix getOzRotateMatrix(double angle);
+};
+
+#endif //MY3D_SRC_SHAPE_H_
