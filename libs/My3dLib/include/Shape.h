ff0262e9ddea8ad16d4f37ea2ae3107508a16063
glhfuck <danyacyrex@gmail.com>
2021-12-19 14:04:08 +0200

    feat: add texture indexes to facet

--- libs/My3dLib/include/Shape.h
+++ libs/My3dLib/include/Shape.h
@@ -7,15 +7,21 @@ class Shape: public Transformable {
   using vector = boost::numeric::ublas::vector<double>;
 
  public:
+  Shape();
+  explicit Shape(const vector& pos);
 
   struct Facet {
-    size_t vertex_idx[3];
-    size_t normal_idx[3];
+    struct VertexInfo {
+      int v_idx;
+      int vt_idx;
+      int vn_idx;
+    };
+    VertexInfo verInfo[3] = {0};
   };
 
-  std::vector<vector> vertices_;
-  std::vector<vector> normals_;
-  std::vector<Facet> facets_;
+  std::vector<vector> vertices;
+  std::vector<vector> normals;
+  std::vector<Facet> facets;
 };
 
 #endif //MY3D_LIBS_MY3DLIB_SRC_SHAPE_H_
--- libs/My3dLib/src/Shape.cpp
+++ libs/My3dLib/src/Shape.cpp
@@ -1,1 +1,7 @@
 #include "../include/Shape.h"
+
+Shape::Shape(const Shape::vector& pos) : Transformable(pos) {
+}
+
+Shape::Shape(): Transformable() {
+}
