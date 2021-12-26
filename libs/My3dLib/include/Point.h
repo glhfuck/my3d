5c8e6e26c96b22989afb3445597c46d854a60d08
glhfuck <danyacyrex@gmail.com>
2021-12-26 21:19:24 +0200

    add: Point struct

---
+++ libs/My3dLib/include/Point.h
@@ -0,0 +1,13 @@
+#ifndef MY3D_LIBS_MY3DLIB_SRC_POINT_H_
+#define MY3D_LIBS_MY3DLIB_SRC_POINT_H_
+
+#include "../include/Color.h"
+
+struct Point {
+  int x = 0;
+  int y = 0;
+  double depth = -1.0;
+  Color color = Color::White;
+};
+
+#endif //MY3D_LIBS_MY3DLIB_SRC_POINT_H_
