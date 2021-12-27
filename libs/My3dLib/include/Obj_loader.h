a0c9d9aed0b9fdcfe7ad9c1b4129219d9899b354
glhfuck <danyacyrex@gmail.com>
2021-12-27 20:04:42 +0200

    feat: minor obj loader improvement

--- libs/My3dLib/include/Obj_loader.h
+++ libs/My3dLib/include/Obj_loader.h
@@ -2,9 +2,11 @@
 #define MY3D_LIBS_MY3DLIB_SRC_OBJ_LOADER_H_
 
 #include <string>
-#include "Shape.h"
+#include <cstdio>
 #include <iostream>
 #include <fstream>
+
+#include "Shape.h"
 
 using vector = boost::numeric::ublas::vector<double>;
 
@@ -13,55 +15,60 @@ class Obj_loader {
   static Shape getShape(const std::string& path);
 
  private:
-  static void handleVertices(Shape& s, std::ifstream& in);
-  static void handleNormals(Shape& s, std::ifstream& in);
-  static void handleFacets(Shape& s, std::ifstream& in);
+  static void handleVertices(Shape& s, FILE* in);
+  static void handleNormals(Shape& s, FILE* in);
+  static void handleFacets(Shape& s, FILE* in);
 };
 
 Shape Obj_loader::getShape(const std::string& path) {
   Shape res;
-  std::ifstream in(path, std::fstream::in);
+  std::FILE* in = fopen(path.c_str(), "r");
 
-  std::string str;
-
-  while (in >> str) {
-    if (str == "v") {
+  while (!feof(in)) {
+    char mode[2] = {0};
+    fscanf(in, "%2s", mode);
+    if (strcmp(mode, "v") == 0) {
       handleVertices(res, in);
-    } else if (str == "vn") {
+    } else  if (strcmp(mode, "vn") == 0) {
       handleNormals(res, in);
-    } else if (str == "f") {
+    } else if (strcmp(mode, "f") == 0) {
       handleFacets(res, in);
     }
   }
 
-  in.close();
+  fclose(in);
   return res;
 }
 
-void Obj_loader::handleVertices(Shape& s, std::ifstream& in) {
+void Obj_loader::handleVertices(Shape& s, FILE* in) {
   vector vertex(4);
-  in >> vertex[0] >> vertex[1] >> vertex[2];
+  fscanf(in, "%lf %lf %lf", &vertex[0], &vertex[1], &vertex[2]);
   vertex[3] = 1;
   s.vertices.emplace_back(vertex);
 }
 
-void Obj_loader::handleNormals(Shape& s, std::ifstream& in) {
+void Obj_loader::handleNormals(Shape& s, FILE* in) {
   vector normal(4);
-  in >> normal[0] >> normal[1] >> normal[2];
+  fscanf(in, "%lf %lf %lf", &normal[0], &normal[1], &normal[2]);
   normal[3] = 0;
   s.normals.emplace_back(normal);
 }
 
-void Obj_loader::handleFacets(Shape& s, std::ifstream& in) {
+void Obj_loader::handleFacets(Shape& s, FILE* in) {
   Shape::Facet facet;
 
-  for (auto& ver_info: facet.verInfo) {
-    char c; // c = '/'    f v/vt/vn
-    in >> ver_info.v_idx >> c >> ver_info.vt_idx >> c >> ver_info.vn_idx;
+  for (auto& ver_info : facet.verInfo) {
+    std::string str;
+    fscanf(in, "%d/", &ver_info.v_idx);
+    fscanf(in, "%d", &ver_info.vt_idx);
+    fscanf(in, "/%d", &ver_info.vn_idx);
+    --ver_info.v_idx;
+    --ver_info.vt_idx;
+    --ver_info.vn_idx;
   }
+
 
   s.facets.emplace_back(facet);
 }
-
 
 #endif //MY3D_LIBS_MY3DLIB_SRC_OBJ_LOADER_H_
